23eaa264cf6f47e2085ea82f7e4183696af28f46
Niklas Baumstark <niklas.baumstark@gmail.com>
2018-08-26 09:42:32 -0700

    simplify

--- main.cpp
+++ main.cpp
@@ -7,39 +7,11 @@
 using namespace std;
 
 // dedicated
-void (*CBaseFileSystem_Constr)(void*);
-void (*CFileSystem_Stdio_Constr)(void*);
-void (*CBaseFileSystem_AddMapPackFile)(void*, const char* pPath, const char* pPathID, int addType);
-void (*AppFactory)();
 void (*DedicatedMain)(int argc, const char** argv);
-void (*set_steam_app)(void*);
-
-void (*CSteamAppSystemGroup_Constr)(void*, int);
-void (*CDedicatedSteamApplication_Constr)(void*, void*);
-void *p_CDedicatedAppSystemGroup_vtable;
-void **p_steam_app;
 
 // engine
 void (*CModelLoader_GetModelForName)(void*, const char* name, int referencetype);
-void (*CDedicatedServerAPI_Connect)(void*, decltype(AppFactory));
-void (*Memory_Init)();
-void** p_g_baseFs;
 void** p_modelloader;
-void** p_materialsystem;
-void** p_g_pDataCache;
-void **p_g_pMaterialSystemHardwareConfig;
-
-// shaderapiempty
-void (*CShaderAPIEmpty_Constr)(void*);
-
-// materialsystem
-void (*CMaterialSystem_Constr1)(void*);
-void** matsystem_p_shader;
-void** matsystem_p_shader2;
-void** matsystem_p_fs;
-
-// datacache
-void* g_dataCache;
 
 template <typename T> void ptr(T*& f, void* so, uint32_t offset) {
     f = (T*)((char*)so + offset);
@@ -90,20 +62,9 @@ int main(int argc, char** argv) {
     lm = (struct link_map*)dlopen("engine.so", RTLD_NOW);
     void* engine = (void*)lm->l_addr;
     assert(engine);
-    lm = (struct link_map*)dlopen("shaderapiempty.so", RTLD_NOW);
-    void* shaderapiempty = (void*)lm->l_addr;
-    assert(shaderapiempty);
-    lm = (struct link_map*)dlopen("materialsystem.so", RTLD_NOW);
-    void* materialsystem = (void*)lm->l_addr;
-    assert(materialsystem);
-    lm = (struct link_map*)dlopen("datacache.so", RTLD_NOW);
-    void* datacache = (void*)lm->l_addr;
-    assert(datacache);
+
     cout << "dedicated.so loaded at " << dedicated << endl;
     cout << "engine.so loaded at " << engine << endl;
-    cout << "shaderapiempty.so loaded at " << shaderapiempty << endl;
-    cout << "materialsystem.so loaded at " << materialsystem << endl;
-    cout << "datacache.so loaded at " << datacache << endl;
 
     mappath = argv[1];
     if (mappath[0] != '/') {
@@ -116,39 +77,10 @@ int main(int argc, char** argv) {
     cout << "Reading from " << mappath << endl;
 
     // dedicated
-    ptr(CBaseFileSystem_Constr, dedicated, 0x3fc50);
-    ptr(CFileSystem_Stdio_Constr, dedicated, 0x4d750);
-    ptr(CBaseFileSystem_AddMapPackFile, dedicated, 0x3def0);
-    ptr(AppFactory, dedicated, 0x7F750);
     ptr(DedicatedMain, dedicated, 0x1beb0);
-
-    ptr(CSteamAppSystemGroup_Constr, dedicated, 0x7F650);
-    ptr(CDedicatedSteamApplication_Constr, dedicated, 0x20710);
-    ptr(set_steam_app, dedicated, 0x80830);
-    ptr(p_CDedicatedAppSystemGroup_vtable, dedicated, 0xAA428);
-    ptr(p_steam_app, dedicated, 0xf8f00);
 
     // engine
     ptr(CModelLoader_GetModelForName, engine, 0x180460);
-    ptr(CDedicatedServerAPI_Connect, engine, 0x2C8690);
-    ptr(Memory_Init, engine, 0x2E3B50);
-    ptr(p_g_baseFs, engine, 0x757080);
-    ptr(p_g_pMaterialSystemHardwareConfig, engine, 0x785C48);
-    ptr(p_g_pDataCache, engine, 0x785BE4);
-    ptr(p_modelloader, engine, 0x6E3C80);
-    ptr(p_materialsystem, engine, 0x785C64);
-
-    // shaderapiempty
-    ptr(CShaderAPIEmpty_Constr, shaderapiempty, 0x92C0);
-
-    // materialsystem
-    ptr(CMaterialSystem_Constr1, materialsystem, 0x76c80);
-    ptr(matsystem_p_shader, materialsystem, 0x189344);
-    ptr(matsystem_p_shader2, materialsystem, 0x18933c);
-    ptr(matsystem_p_fs, materialsystem, 0x1a00cc);
-
-    // datacache
-    ptr(g_dataCache, datacache, 0xdaf80);
 
     const char* args[] = {"x", "-game", "csgo", "-nominidumps", "-nobreakpad"};
     DedicatedMain(sizeof args / sizeof *args, args);
--- patch.py
+++ patch.py
@@ -16,15 +16,21 @@ def patch(d, offset, s):
 ######### engine.so
 dat = bytearray(open("bin/engine.orig.so").read())
 
-# Jump to forkserver entry point after initialization sequence is complete (enough)
+# Jump to forkserver entry point after initialization.
+# 0x286d20 is the NET_CloseAllSockets function.
 patch(dat, 0x286D20,
     (
     '\xb8' + struct.pack('<I', startpoint) +  # mov eax, startpoint
     '\xff\xd0'                                # call eax
     ))
+
+# Patch out a function that is registered via atexit().
+# You can find it by looking for the single xref to the string
+# "Missing shutdown function for %s"
 patch(dat, 0x2CCDB0, "\xc3")
 
-# nop out call to BeginWatchdogTimer, so that we don't get SIGABRT after a while
+# nop out a call to BeginWatchdogTimer in the initialization sequence, so that we
+# don't get SIGABRT after a while
 patch(dat, 0x263F5F, "\x90"*6)
 
 with open("bin/engine.so", "wb") as f:
@@ -33,7 +39,10 @@ with open("bin/engine.so", "wb") as f:
 ######### libtier0.so
 dat = bytearray(open("bin/libtier0.orig.so").read())
 
-# Plat_ExitPlatform should not crash
+# Avoid Plat_ExitPlatform crash
+# .text:000156E7                 test    ebx, ebx
+# .text:000156E9                 jz      short loc_156F5
+# .text:000156EB                 mov     ds:dword_0, 1
 patch(dat, 0x156E7, "\x90"*(0x156F5-0x156e7))
 with open("bin/libtier0.so", "wb") as f:
   f.write(dat)
