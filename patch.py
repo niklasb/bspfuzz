6ee0cd16faf9ace0cd2fdfd443ec2dcdf3dbcb71
Swapnadeep Som <75885103+Dying-Doomer@users.noreply.github.com>
2021-07-30 18:34:34 +0530

    added newer offsets to the code

--- patch.py
+++ patch.py
@@ -18,7 +18,7 @@ dat = bytearray(open("bin/engine.orig.so", "rb").read())
 
 # Jump to forkserver entry point after initialization.
 # 0x286d20 is the NET_CloseAllSockets function.
-patch(dat, 0x286D20,
+patch(dat, 0x29E6B0,
 (
   b'\xb8' + struct.pack('<I', startpoint) +  # mov eax, startpoint
   b'\xff\xd0'                                # call eax
@@ -27,11 +27,11 @@ patch(dat, 0x286D20,
 # Patch out a function that is registered via atexit().
 # You can find it by looking for the single xref to the string
 # "Missing shutdown function for %s"
-patch(dat, 0x2CCDB, b"\xc3")
+patch(dat, 0x2E5900, b"\xc3")
 
 # nop out a call to BeginWatchdogTimer in the initialization sequence, so that we
 # don't get SIGABRT after a while
-patch(dat, 0x263F5F, b"\x90"*6)
+patch(dat, 0x27459F, b"\x90"*6)
 
 with open("bin/engine.so", "wb") as f:
   f.write(dat)
@@ -43,6 +43,6 @@ dat = bytearray(open("bin/libtier0.orig.so", "rb").read())
 # .text:000156E7                 test    ebx, ebx
 # .text:000156E9                 jz      short loc_156F5
 # .text:000156EB                 mov     ds:dword_0, 1
-patch(dat, 0x156E7, b"\x90"*(0x156F5-0x156e7))
+patch(dat, 0x15877, b"\x90"*(0x15885 - 0x15877))
 with open("bin/libtier0.so", "wb") as f:
   f.write(dat)
